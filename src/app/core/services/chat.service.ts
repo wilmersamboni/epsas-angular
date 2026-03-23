7f2e9d1b9b433ecb4a3122e9d13af91ec2612251
d-09927 <danielas09927@gmail.com>
2026-03-22 21:20:57 -0500

    re realizo la conexion de asistente virtual no n8n y se le agrego el temporizador

--- src/app/core/services/chat.service.ts
+++ src/app/core/services/chat.service.ts
@@ -7,16 +7,20 @@ import { Observable } from 'rxjs';
 })
 export class ChatService {
 
-  private webhookUrl = 'https://n8n.falla.lat/webhook-test/asistente';
+  private webhookUrl = 'https://bot.kromas.lat/webhook/asistente';
+  private sessionId = crypto.randomUUID();
 
   constructor(private http: HttpClient) {}
 
-  // Llamada al webhook de n8n
+  // Genera nuevo sessionId → n8n empieza conversación desde cero
+  resetSession() {
+    this.sessionId = crypto.randomUUID();
+  }
+
   sendMessage(userMessage: string): Observable<any> {
-    return this.http.post<any>(
-      this.webhookUrl,
-      { mensaje: userMessage },
-      { withCredentials: false }  // <-- aquí le decimos al navegador que no envíe cookies
-    );
+    return this.http.post<any>(this.webhookUrl, {
+      message: userMessage,
+      sessionId: this.sessionId
+    });
   }
 }
